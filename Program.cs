005457bc463cca464f54dcb08778ad2a31866851
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-10 12:55:00 +0300

    update solution and read.me

--- Program.cs
+++ Program.cs
@@ -2,9 +2,55 @@
 {
 	internal class Program
 	{
+		public class MaximumValueGivenIndexBoundedArray
+		{
+			private long GetSum(int index, int value, int n)
+			{
+				long count = 0;
+				if (value < index)
+				{
+					count += (long)(value + value - index) * (index + 1) / 2;
+				}
+				else
+				{
+					count += (long)(value + 1) * value / 2 + index - value + 1;
+				}
+				if (value >= n - index)
+				{
+					count += (long)(value + value - n + 1 + index) * (n - index) / 2;
+				}
+				else
+				{
+					count += (long)(value + 1) * value / 2 + n - index - value;
+				}
+				return count - value;
+			}
+
+			public int MaxValue(int n, int index, int maxSum)
+			{
+				int left = 1;
+				int right = maxSum;
+				while(left < right)
+				{
+					int middle = (left + right + 1) / 2;
+					if (GetSum(index, middle, n) <= maxSum)
+					{
+						left = middle;
+					}
+					else
+					{
+						right = middle - 1;
+					}
+				}
+				return left;
+			}
+		}
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			MaximumValueGivenIndexBoundedArray maximumValueGivenIndexBoundedArray = new();
+            Console.WriteLine(maximumValueGivenIndexBoundedArray.MaxValue(4, 2, 6));
+			Console.WriteLine(maximumValueGivenIndexBoundedArray.MaxValue(6, 1, 10));
 		}
 	}
 }
